50dfac89a5d3b81fe77af056cf977aab6592d658
Jason Davies <jason@jasondavies.com>
2012-03-20 10:41:45 +0000

    Support non-byte-aligned radixes.
    Also make radix size configurable (default is 11 bits).

--- radixsort.js
+++ radixsort.js
@@ -3,119 +3,241 @@
 (function(exports) {
   exports.sort = radixsort;
 
-  var radixBits = 8,
-      maxRadix = 1 << radixBits,
-      msbMask = maxRadix >> 1,
-      radixMask = maxRadix - 1,
-      histograms = radixsort._histograms = new Int32Array(maxRadix * Math.ceil(64 / radixBits));
+  var radixBits,
+      maxRadix,
+      radixMask,
+      histograms;
 
   function radixsort() {
     function sort(array, sorted) {
-      var f64 = array instanceof Float64Array,
-          floating = array instanceof Float32Array || f64,
+      var floating = array instanceof Float32Array || array instanceof Float64Array,
           signed = !(array instanceof Uint32Array || array instanceof Uint16Array || array instanceof Uint8Array),
-          n = array.length,
           input = floating ? new Int32Array(array.buffer) : array,
+          n = input.length,
           passCount = Math.ceil(array.BYTES_PER_ELEMENT * 8 / radixBits),
+          maxOffset = maxRadix * (passCount - 1),
+          msbMask = 1 << ((array.BYTES_PER_ELEMENT * 8 - 1) % radixBits),
+          lastMask = (msbMask << 1) - 1,
           tmp,
-          i;
+          start,
+          inner,
+          end,
+          histogram;
+
+      if (array instanceof Float32Array) {
+        start = startFloat32;
+        inner = innerFloat32;
+        end = endFloat32;
+        histogram = histogramFloat32;
+      } else if (array instanceof Float64Array) {
+        start = startFloat64;
+        inner = innerFloat64;
+        end = endFloat64;
+        histogram = histogramFloat64;
+      } else if (array instanceof Uint32Array || array instanceof Uint16Array || array instanceof Uint8Array) {
+        start = startUint;
+        inner = end = innerUint;
+        histogram = histogramUint;
+      } else {
+        start = startInt;
+        inner = innerInt;
+        end = endInt;
+        histogram = histogramInt;
+      }
+
       sorted = sorted ? floating ? new Int32Array(sorted.buffer) : sorted
           : new input.constructor(input.length);
 
-      createHistograms(input, passCount, signed, floating, f64);
-
-      var maxPass = (passCount - 1) * radixBits;
-      for (var pass = 0, offset = 0; pass <= maxPass; pass += radixBits, offset += maxRadix) {
-        for (i = 0; i < n; i++) {
-          var x,
-              d,
-              e;
-          if (f64) {
-            e = input[d = i << 1];
-            d = input[d + 1];
-            x = (pass > 31 ? d >> (pass - 32) : e >> pass) & radixMask;
-          } else {
-            e = 0;
-            d = input[i];
-            x = (d >> pass) & radixMask;
-          }
-          if (signed) {
-            if (pass === maxPass) {
-              if (floating) {
-                if (x & msbMask) {
-                  d ^= 0x80000000;
-                } else {
-                  d ^= 0xffffffff;
-                  e ^= 0xffffffff;
-                }
-              } else x ^= msbMask;
-            } else if (pass === 0 && floating) {
-              if (d >> 31) {
-                d ^= 0xffffffff;
-                e ^= 0xffffffff;
-                x ^= radixMask;
-              } else {
-                d ^= 0x80000000;
-              }
-            }
-          }
-          x = ++histograms[offset + x];
-          if (f64) {
-            sorted[x <<= 1] = e;
-            x++;
-          }
-          sorted[x] = d;
+      for (var i = 0, n = maxRadix * passCount; i < n; i++) histograms[i] = 0;
+      histogram(input, maxOffset, lastMask);
+      for (var j = 0; j <= maxOffset; j += maxRadix) {
+        for (var id = j, sum = 0; id < j + maxRadix; id++) {
+          var tsum = histograms[id] + sum;
+          histograms[id] = sum - 1;
+          sum = tsum;
         }
+      }
+
+      var pass = 0;
+      passCount--;
+      if (passCount) {
+        start(input, sorted);
         tmp = sorted;
         sorted = input;
         input = tmp;
+        while (++pass < passCount) {
+          inner(input, sorted, pass);
+          tmp = sorted;
+          sorted = input;
+          input = tmp;
+        }
       }
-      return input.buffer;
+      end(input, sorted, pass, msbMask);
+      return sorted.buffer;
     }
 
-    return sort;
+    sort.radix = function(_) {
+      maxRadix = 1 << (radixBits = +_);
+      radixMask = maxRadix - 1;
+      histograms = radixsort._histograms = new Int32Array(maxRadix * Math.ceil(64 / radixBits));
+      return sort;
+    };
+
+    return sort.radix(11);
   }
 
-  function createHistograms(input, passCount, signed, floating, f64) {
-    var i,
-        j,
-        k,
-        id,
-        sum,
-        tsum,
-        n = input.length,
-        maxOffset = maxRadix * (passCount - 1),
-        msbShift = radixBits * (passCount - 1),
-        d,
-        e,
-        x,
-        mask;
-    if (f64) msbShift -= 32;
-    for (i = 0; i < maxRadix * passCount; i++) histograms[i] = 0;
-    for (i = 0; i < n; i++) {
-      // Check sign bit first.
-      if (f64) {
-        e = input[i];
-        d = input[++i];
-      } else d = e = input[i];
-      x = (d >>> msbShift) & radixMask;
-      mask = floating ? x & msbMask ? (x ^= radixMask, radixMask) : (x |= msbMask, 0) : 0;
-      if (!floating && signed) x ^= msbMask;
-      histograms[maxOffset + x]++;
-      for (j = 0, k = 0; j < maxOffset; j += maxRadix, k += radixBits) {
-        if (k >= 32) {
-          k -= 32;
-          e = d;
-        }
-        histograms[j + (((e >>> k) & radixMask) ^ mask)]++;
+  function startInt(input, sorted) {
+    for (var i = 0, n = input.length; i < n; i++) {
+      var d = input[i];
+      sorted[++histograms[d & radixMask]] = d;
+    }
+  }
+
+  function innerInt(input, sorted, pass) {
+    for (var i = 0, n = input.length, offset = pass * maxRadix, s = pass * radixBits; i < n; i++) {
+      var d = input[i];
+      sorted[++histograms[offset + (d >>> s & radixMask)]] = d;
+    }
+  }
+
+  function endInt(input, sorted, pass, msbMask) {
+    var lastMask = (msbMask << 1) - 1;
+    for (var i = 0, n = input.length, offset = pass * maxRadix, s = pass * radixBits; i < n; i++) {
+      var d = input[i];
+      sorted[++histograms[offset + (d >>> s & lastMask ^ msbMask)]] = d;
+    }
+  }
+
+  function startUint(input, sorted) {
+    for (var i = 0, n = input.length; i < n; i++) {
+      var d = input[i];
+      sorted[++histograms[d & radixMask]] = d;
+    }
+  }
+
+  function innerUint(input, sorted, pass) {
+    for (var i = 0, n = input.length, offset = pass * maxRadix, s = pass * radixBits; i < n; i++) {
+      var d = input[i],
+          x = d >>> s & radixMask;
+      sorted[++histograms[offset + x]] = d;
+    }
+  }
+
+  function startFloat32(input, sorted) {
+    for (var i = 0, n = input.length; i < n; i++) {
+      var d = input[i];
+      // Fast check for most-significant bit: signed-shift of a negative value results in 0xffffffff.
+      d ^= (d >> 31) | 0x80000000;
+      sorted[++histograms[d & radixMask]] = d;
+    }
+  }
+
+  function innerFloat32(input, sorted, pass) {
+    for (var i = 0, n = input.length, offset = pass * maxRadix, s = pass * radixBits; i < n; i++) {
+      var d = input[i];
+      sorted[++histograms[offset + (d >>> s & radixMask)]] = d;
+    }
+  }
+
+  function endFloat32(input, sorted, pass, msbMask) {
+    var lastMask = (msbMask << 1) - 1;
+    for (var i = 0, n = input.length, offset = pass * maxRadix, s = pass * radixBits; i < n; i++) {
+      var d = input[i],
+          x = (d >>> s) & lastMask;
+      sorted[++histograms[offset + x]] = d ^ (x & msbMask ? 0x80000000 : 0xffffffff);
+    }
+  }
+
+  function startFloat64(input, sorted) {
+    for (var i = 0, n = input.length; i < n; i++) {
+      var e = input[i],
+          d = input[++i],
+          x = (d >> 31);
+      d ^= x | 0x80000000;
+      e ^= x;
+      x = ++histograms[e & radixMask] << 1;
+      sorted[x++] = e;
+      sorted[x] = d;
+    }
+  }
+
+  function innerFloat64(input, sorted, pass) {
+    for (var i = 0, n = input.length, offset = pass * maxRadix, s = pass * radixBits; i < n; i++) {
+      var e = input[i],
+          d = input[++i],
+          x = (s <= 32 - radixBits ? e >>> s
+              : s >= 32 ? d >>> (s - 32)
+              : (d << (32 - s)) | (e >>> s)) & radixMask;
+      x = ++histograms[offset + x] << 1;
+      sorted[x++] = e;
+      sorted[x] = d;
+    }
+  }
+
+  function endFloat64(input, sorted, pass, msbMask) {
+    var lastMask = (msbMask << 1) - 1;
+    for (var i = 0, n = input.length, offset = pass * maxRadix, s = pass * radixBits - 32; i < n; i++) {
+      var e = input[i],
+          d = input[++i],
+          x = d >>> s & lastMask;
+      if (x & msbMask) {
+        d ^= 0x80000000;
+      } else {
+        d ^= 0xffffffff;
+        e ^= 0xffffffff;
+      }
+      x = ++histograms[offset + x] << 1;
+      sorted[x++] = e;
+      sorted[x] = d;
+    }
+  }
+
+  function histogramInt(input, maxOffset, lastMask) {
+    var msbMask = (lastMask + 1) >>> 1;
+    for (var i = 0, n = input.length; i < n; i++) {
+      var d = input[i];
+      for (var j = 0, k = 0; j < maxOffset; j += maxRadix, k += radixBits) {
+        histograms[j + (d >>> k & radixMask)]++;
+      }
+      histograms[j + (d >>> k & lastMask ^ msbMask)]++;
+    }
+  }
+
+  function histogramUint(input, maxOffset) {
+    for (var i = 0, n = input.length; i < n; i++) {
+      var d = input[i];
+      for (var j = 0, k = 0; j <= maxOffset; j += maxRadix, k += radixBits) {
+        histograms[j + (d >>> k & radixMask)]++;
       }
     }
-    for (j = 0; j <= maxOffset; j += maxRadix) {
-      for (id = j, sum = 0; id < j + maxRadix; id++) {
-        tsum = histograms[id] + sum;
-        histograms[id] = sum - 1;
-        sum = tsum;
+  }
+
+  function histogramFloat32(input, maxOffset, lastMask) {
+    for (var i = 0, n = input.length; i < n; i++) {
+      var d = input[i];
+      d ^= (d >> 31) | 0x80000000;
+      for (var j = 0, k = 0; j < maxOffset; j += maxRadix, k += radixBits) {
+        histograms[j + (d >>> k & radixMask)]++;
       }
+      histograms[j + (d >>> k & lastMask)]++;
+    }
+  }
+
+  function histogramFloat64(input, maxOffset, lastMask) {
+    for (var i = 0, n = input.length; i < n; i++) {
+      var e = input[i],
+          d = input[++i],
+          x = d >> 31;
+      d ^= x | 0x80000000;
+      e ^= x;
+      for (var j = 0, k = 0; k <= 32 - radixBits; j += maxRadix, k += radixBits) {
+        histograms[j + (e >>> k & radixMask)]++;
+      }
+      histograms[j + (((d << (32 - k)) | (e >>> k)) & radixMask)]++;
+      for (k += radixBits - 32, j += maxRadix; j < maxOffset; j += maxRadix, k += radixBits) {
+        histograms[j + (d >>> k & radixMask)]++;
+      }
+      histograms[j + (d >>> k & lastMask)]++;
     }
   }
 })(typeof exports !== "undefined" ? exports : this);
