5b88b2ef8138061b28c50eb5fa44f620ec72ff12
Jason Davies <jason@jasondavies.com>
2012-03-20 10:29:59 +0000

    More performance improvements.
    It turns out that Int32Array is considerably faster than Uint32Array, so
    we use this instead for floats.  The code is now also slightly more
    generic, in preparation for other radix sizes.

--- radixsort.js
+++ radixsort.js
@@ -5,23 +5,24 @@
 
   var radixBits = 8,
       maxRadix = 1 << radixBits,
-      histograms = radixsort._histograms = new Int32Array(maxRadix * 8);
+      msbMask = maxRadix >> 1,
+      radixMask = maxRadix - 1,
+      histograms = radixsort._histograms = new Int32Array(maxRadix * Math.ceil(64 / radixBits));
 
   function radixsort() {
     function sort(array, sorted) {
-      var floating = array instanceof Float32Array || (f64 = array instanceof Float64Array),
+      var f64 = array instanceof Float64Array,
+          floating = array instanceof Float32Array || f64,
           signed = !(array instanceof Uint32Array || array instanceof Uint16Array || array instanceof Uint8Array),
           n = array.length,
-          input = floating ? new Uint32Array(array.buffer) : array,
-          inputBytes = new Uint8Array(input.buffer),
-          passCount = array.BYTES_PER_ELEMENT,
-          f64,
+          input = floating ? new Int32Array(array.buffer) : array,
+          passCount = Math.ceil(array.BYTES_PER_ELEMENT * 8 / radixBits),
           tmp,
           i;
-      sorted = sorted ? floating ? new Uint32Array(sorted.buffer) : sorted
+      sorted = sorted ? floating ? new Int32Array(sorted.buffer) : sorted
           : new input.constructor(input.length);
 
-      createHistograms(inputBytes, passCount, signed, floating);
+      createHistograms(input, passCount, signed, floating, f64);
 
       var maxPass = (passCount - 1) * radixBits;
       for (var pass = 0, offset = 0; pass <= maxPass; pass += radixBits, offset += maxRadix) {
@@ -32,27 +33,27 @@
           if (f64) {
             e = input[d = i << 1];
             d = input[d + 1];
-            x = (pass > 31 ? d >> (pass - 32) : e >> pass) & 0xff;
+            x = (pass > 31 ? d >> (pass - 32) : e >> pass) & radixMask;
           } else {
             e = 0;
             d = input[i];
-            x = (d >> pass) & 0xff;
+            x = (d >> pass) & radixMask;
           }
           if (signed) {
             if (pass === maxPass) {
               if (floating) {
-                if (x >>> 7) {
+                if (x & msbMask) {
                   d ^= 0x80000000;
                 } else {
                   d ^= 0xffffffff;
                   e ^= 0xffffffff;
                 }
-              } else x ^= 0x80;
+              } else x ^= msbMask;
             } else if (pass === 0 && floating) {
               if (d >> 31) {
                 d ^= 0xffffffff;
                 e ^= 0xffffffff;
-                x ^= 0xff;
+                x ^= radixMask;
               } else {
                 d ^= 0x80000000;
               }
@@ -75,28 +76,42 @@
     return sort;
   }
 
-  function createHistograms(inputBytes, passCount, signed, floating) {
+  function createHistograms(input, passCount, signed, floating, f64) {
     var i,
         j,
+        k,
         id,
         sum,
         tsum,
-        n = inputBytes.length;
+        n = input.length,
+        maxOffset = maxRadix * (passCount - 1),
+        msbShift = radixBits * (passCount - 1),
+        d,
+        e,
+        x,
+        mask;
+    if (f64) msbShift -= 32;
     for (i = 0; i < maxRadix * passCount; i++) histograms[i] = 0;
-    for (i = 0; i < n;) {
+    for (i = 0; i < n; i++) {
       // Check sign bit first.
-      var x = inputBytes[i + passCount - 1],
-          mask = floating ? x >>> 7 ? (x ^= 0xff, 0xff) : (x |= 0x80, 0) : 0;
-      if (!floating && signed) x ^= 0x80;
-      histograms[((passCount - 1) << radixBits) + x]++;
-      for (j = 0; j < passCount - 1; j++) {
-        x = inputBytes[i++] ^ mask;
-        histograms[(j << radixBits) + x]++;
+      if (f64) {
+        e = input[i];
+        d = input[++i];
+      } else d = e = input[i];
+      x = (d >>> msbShift) & radixMask;
+      mask = floating ? x & msbMask ? (x ^= radixMask, radixMask) : (x |= msbMask, 0) : 0;
+      if (!floating && signed) x ^= msbMask;
+      histograms[maxOffset + x]++;
+      for (j = 0, k = 0; j < maxOffset; j += maxRadix, k += radixBits) {
+        if (k >= 32) {
+          k -= 32;
+          e = d;
+        }
+        histograms[j + (((e >>> k) & radixMask) ^ mask)]++;
       }
-      i++;
     }
-    for (j = 0; j < passCount; j++) {
-      for (i = 0, id = j << radixBits, sum = 0; i < maxRadix; i++, id++) {
+    for (j = 0; j <= maxOffset; j += maxRadix) {
+      for (id = j, sum = 0; id < j + maxRadix; id++) {
         tsum = histograms[id] + sum;
         histograms[id] = sum - 1;
         sum = tsum;
